f0003f32ae27dd47e9f826b6124ca15953d47dfd
lalit10 <lalitbangad@gmail.com>
2021-09-02 23:11:39 -0400

    Setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='N th Fibonacci Number',
+    version='1.0',
+    description='This program provides nth fibonacci number.',
+    author='Lalit Bangad, Anirudh Pande, Pratyush Vaidya',
+    author_email='llbangad@ncsu.edu, apande@ncsu.edu,pavaidya@ncsu.edu',
+    url='https://github.com/lalit10/CSC510-Group19',
+    packages=[],    
+    classifiers=[
+        "License :: OSI Approved :: MIT",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Engineering",
+    ],
+    keywords='',
+    license='MIT',
+    install_requires=[],
+)
